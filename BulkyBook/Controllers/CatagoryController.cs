67acd69517607c2f4185130b2fdec5d47e519e9c
tu-sh-ar <tushar.g@codestoresolutions.com>
2022-01-25 12:39:08 +0530

    Added DbFething Code

---
+++ BulkyBook/Controllers/CatagoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BulkyBook.Data;
+using BulkyBook.Models;
+
+namespace BulkyBook.Controllers
+{
+    public class CatagoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CatagoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Catagory> objCatagoryList = _db.Catagories;
+            return View(objCatagoryList);
+        }
+    }
+}
